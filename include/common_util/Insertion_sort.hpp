8d820f2a88b95938c34efabdb493b77d79efe8a8
Jacob Schloss <jacob@schloss.io>
2019-03-11 00:06:30 -0500

    start insertion sort

---
+++ include/common_util/Insertion_sort.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef<class Iter, class Comp>
+void insertion_sort(Iter begin, Iter end, Comp comp)
+{
+	Iter i = begin;
+	Iter j = begin;
+	for(Iter i = begin; i != end; ++i)
+	{
+		if(comp())
+	}
+
+}
