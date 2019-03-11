7d04ff6c33ca620f89d072e92ccee92760799970
Jacob Schloss <jacob@schloss.io>
2019-03-10 23:18:59 -0700

    refactor

--- include/common_util/Insertion_sort.hpp
+++ include/common_util/Insertion_sort.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
-typedef<class Iter, class Comp>
-void insertion_sort(Iter begin, Iter end, Comp comp)
+#include <functional>
+#include <iterator>
+#include <utility>
+
+
+template<class Iter, class Comp>
+void insertion_sort(Iter begin, Iter end, Comp comp_lt)
 {
 	Iter i = begin;
-	Iter j = begin;
-	for(Iter i = begin; i != end; ++i)
+	++i;
+	for(; i != end; ++i)
 	{
-		if(comp())
+		for(Iter j = i; (j != begin) && comp_lt(*j, *std::prev(j)); --j)
+		{
+			std::iter_swap(j, std::prev(j));
+		}
 	}
+}
 
+template<class Iter>
+void insertion_sort(Iter begin, Iter end)
+{
+	insertion_sort(begin, end, std::less< typename std::iterator_traits<Iter>::value_type >());
 }
---
+++ src/Insertion_sort.cpp
@@ -0,0 +1,1 @@
+#include "common_util/Insertion_sort.hpp"
