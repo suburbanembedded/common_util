78a1f0b90ef95434a4a74cdd3ff62cc05716e8f8
Jacob Schloss <jacob@schloss.io>
2019-05-14 18:12:41 -0700

    copy info

--- src/Byte_util.cpp
+++ src/Byte_util.cpp
@@ -1,3 +1,10 @@
+/**
+ * @brief Byte_util
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2018-2019 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #include "common_util/Byte_util.hpp"
 
 constexpr char Byte_util::nibble_hex_lut[];
--- src/Insertion_sort.cpp
+++ src/Insertion_sort.cpp
@@ -1,1 +1,8 @@
+/**
+ * @brief insertion_sort
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2018 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #include "common_util/Insertion_sort.hpp"
