c1e3469b505e89224406f6d76115b3fd92574bb3
Jacob Schloss <jacob@schloss.io>
2020-07-23 11:26:39 -0700

    build installer

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -7,16 +7,16 @@ set -v
 CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/common_util/common_util:${GITHUB_REF##*/}  /bin/bash)
 docker start $CONTAINER_ID
 docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
-docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/debug/
-docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/release/
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make package -j`nproc` -C build/debug/
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make package -j`nproc` -C build/release/
 docker stop $CONTAINER_ID
 
 pushd $GITHUB_WORKSPACE/build/debug/
-sha256sum -b libcommon_util.a | tee sha256.txt
+sha256sum -b libcommon_util.a common_util-0.1.0-Linux.deb | tee sha256.txt
 tar -czf $GITHUB_WORKSPACE/common_util-debug-$GITHUB_SHA.tar.gz    libcommon_util.a sha256.txt
 popd
 
 pushd $GITHUB_WORKSPACE/build/release/
-sha256sum -b libcommon_util.a | tee sha256.txt
+sha256sum -b libcommon_util.a common_util-0.1.0-Linux.deb | tee sha256.txt
 tar -czf $GITHUB_WORKSPACE/common_util-release-$GITHUB_SHA.tar.gz  libcommon_util.a sha256.txt
 popd
