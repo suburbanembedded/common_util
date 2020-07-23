9701b9bfa953658e79f876b4de8bb25a450c7a7e
Jacob Schloss <jacob@schloss.io>
2020-07-23 10:54:40 -0700

    test ci

---
+++ scripts/build_ci.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+trap 'exit -1' err
+
+set -v
+
+CONTAINER_ID=$(docker create -v $GITHUB_WORKSPACE:/tmp/workspace -it docker.pkg.github.com/suburbanembedded/common_util/common_util:${GITHUB_REF##*/}  /bin/bash)
+docker start $CONTAINER_ID
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID ./generate_cmake.sh
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/debug/
+docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/release/
+docker stop $CONTAINER_ID
+
+pushd $GITHUB_WORKSPACE/build/debug/
+sha256sum -b libcommon_util.a | tee sha256.txt
+tar -czf $GITHUB_WORKSPACE/common_util-debug-$GITHUB_SHA.tar.gz    libcommon_util.a sha256.txt
+popd
+
+pushd $GITHUB_WORKSPACE/build/release/
+sha256sum -b libcommon_util.a | tee sha256.txt
+tar -czf $GITHUB_WORKSPACE/common_util-release-$GITHUB_SHA.tar.gz  libcommon_util.a sha256.txt
+popd
